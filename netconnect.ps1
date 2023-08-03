6741b97ec9d5779139e65c1f89c412837e27a273
Teimouri <davoodteymoori@gmail.com>
2023-08-03 20:29:45 +0330

    Add some new options

--- netconnect.ps1
+++ netconnect.ps1
@@ -1,98 +1,90 @@
-#!/usr/bin/perl
+param (
+    [switch]$Help,
+    [int[]]$CreatePorts,
+    [string]$TestServers,
+    [int[]]$TestPorts
+)
 
-use strict;
-use Getopt::Long;
-
-sub usage {
-    print <<"END_USAGE";
+function Show-Usage {
+    $usage = @"
 NetConnect - TCP/UDP Connection Testing Tool
 
-Usage: $0 [options]
+Usage: netconnect.ps1 [options]
 
 Options:
   -h, --help            Show this help message and exit.
-  -c PORT_1 PORT_2 ...  Create TCP/UDP ports on the local computer.
+  -c PORT_1,PORT_2,...  Create TCP/UDP ports on the local computer.
   -t SERVER_RANGE       Test connections with multiple servers or a range of IP addresses.
-  -p PORT_1 PORT_2 ...  Ports to test connections with.
+  -p PORT_1,PORT_2,...  Ports to test connections with.
 
 Examples:
   Create ports 8080 and 9000 on the local computer:
-  $0 -c 8080 9000
+  netconnect.ps1 -c 8080,9000
 
   Test connectivity to ports 22, 80, and 443 on multiple servers:
-  $0 -t 192.168.1.100-203.0.113.10 -p 22 80 443
-END_USAGE
+  netconnect.ps1 -t 192.168.1.100-203.0.113.10 -p 22,80,443
+"@
+    Write-Output $usage
 }
 
-sub create_ports {
-    print "Creating TCP/UDP ports on the local computer...\n";
+function Create-Ports {
+    Write-Output "Creating TCP/UDP ports on the local computer..."
     # Implement the logic to create ports here
-    foreach my $port (@_) {
-        print "Port $port created.\n";
+    foreach ($port in $CreatePorts) {
+        Write-Output "Port $port created."
     }
 }
 
-sub test_connections {
-    print "Testing connections...\n";
+function Test-Connections {
+    Write-Output "Testing connections..."
     # Implement the logic to test connections here
-    foreach my $server (@servers) {
-        foreach my $port (@ports) {
-            print "Testing connectivity to $server:$port ...\n";
+    foreach ($server in $TestServers) {
+        foreach ($port in $TestPorts) {
+            Write-Output "Testing connectivity to $server:$port ..."
             # Implement the logic to test connection to each server and port here
             # Output success or failure messages accordingly
         }
     }
 }
 
-my ( $help, @ports, $server_range );
-GetOptions(
-    'h|help' => \$help,
-    'c=s{1,}' => sub { create_ports(@ARGV) and exit },
-    't=s' => \$server_range,
-    'p=s{1,}' => \@ports,
-);
-
-if ($help) {
-    usage();
-    exit 0;
+if ($Help) {
+    Show-Usage
+    exit
 }
 
 # Check if both create and test options are used together
-if ( defined $server_range && scalar @ports > 0 ) {
-    print "Error: Cannot use create ports and test options together.\n";
-    usage();
-    exit 1;
+if ($CreatePorts -and $TestServers) {
+    Write-Output "Error: Cannot use create ports and test options together."
+    Show-Usage
+    exit 1
 }
 
 # Check if either create or test options are used
-if ( !defined $server_range && scalar @ports == 0 ) {
+if (-not $CreatePorts -and -not $TestServers) {
     # No options provided, run the interactive menu
-    print "Interactive menu is not yet implemented. Use command-line options instead.\n";
-    exit 1;
+    Write-Output "Interactive menu is not yet implemented. Use command-line options instead."
+    exit 1
 }
 
 # Check if test option is used without specifying ports
-if ( defined $server_range && scalar @ports == 0 ) {
-    print "Error: Test option requires specifying ports with the -p option.\n";
-    usage();
-    exit 1;
+if ($TestServers -and (-not $TestPorts -or $TestPorts.Count -eq 0)) {
+    Write-Output "Error: Test option requires specifying ports with the -p option."
+    Show-Usage
+    exit 1
 }
 
 # Check if test option is used without specifying servers
-if ( !defined $server_range && scalar @ports > 0 ) {
-    print "Error: Test option requires specifying servers with the -t option.\n";
-    usage();
-    exit 1;
+if (-not $TestServers -and $TestPorts) {
+    Write-Output "Error: Test option requires specifying servers with the -t option."
+    Show-Usage
+    exit 1
 }
 
-# Split server_range into start and end IPs
-my ( $start_ip, $end_ip ) = split /-/, $server_range;
-
-# Generate the array of servers from the range
-use Net::CIDR::Lite;
-my $cidr = Net::CIDR::Lite->new;
-$cidr->add_range($start_ip, $end_ip);
-my @servers = $cidr->list();
-
-# Perform the test connections
-test_connections();
+if ($CreatePorts) {
+    Create-Ports
+}
+elseif ($TestServers -and $TestPorts) {
+    $start_ip, $end_ip = $TestServers -split '-'
+    $servers = [ipaddress.IPAddress]::Parse($start_ip)..[ipaddress.IPAddress]::Parse($end_ip) | ForEach-Object { $_.ToString() }
+    Test-Connections
+}
